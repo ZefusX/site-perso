8f118eb35bad31351e0e054d889cb0db9468ebd1
ZefusX <zefuscode@gmail.com>
2024-04-17 12:13:08 +0200

    3 : Headers

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "Blog de Theo",
+    url: "https://example.com",
+    description: "Blog en Nextjs 14 en utilisant velite, tailwind and shadcn",
+    author: "ZefusX",
+    links: {
+        github: "https://github.com/ZefusX",
+    },   
+}
+
+export type SiteConfig = typeof siteConfig
